44fec0645e04182a867e49e838fea906f179380e
Heisen <ariya8.narayan7@outlook.jp>
2022-03-08 22:04:30 +0900

    Established search results page

--- render.py
+++ render.py
@@ -4,9 +4,10 @@ import data_
 
 app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return render_template('index.html')
+@app.route('/search')
+def search():
+    videos = data_.get_results_from_keyword(keyword = 'dude perfect')
+    return render_template('search.html', videos = videos)
 
 if __name__ == '__main__':
     app.run()
